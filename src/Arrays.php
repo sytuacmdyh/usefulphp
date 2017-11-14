ce801d10856d796ddce91f86ea209bec4bf8ea7b
dyh <yzcczdyh@gmail.com>
2017-11-14 21:49:34 +0800

    init

---
+++ src/Arrays.php
@@ -0,0 +1,23 @@
+<?php
+namespace Dyh;
+
+class Arrays
+{
+    /**
+     * 二维数组根据字段进行排序
+     *
+     * @param array $array 需要排序的数组
+     * @param string $field 排序的字段
+     * @param string $sort 排序顺序标志 SORT_DESC 降序；SORT_ASC 升序
+     * @return mixed
+     */
+    function arraySort(&$array, $field, $sort = 'SORT_DESC')
+    {
+        $arrSort = array();
+        foreach ($array as $uniqid => $row) {
+            $arrSort[$field][$uniqid] = $row[$field];
+        }
+        array_multisort($arrSort[$field], constant($sort), $array);
+        return $array;
+    }
+}
